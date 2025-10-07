23b23cc28ef037ed63f1c0255a0e65afe5262184
07-Bagaskara <bagaskarapradana36@gmail.com>
2025-10-07 09:52:10 +0700

    Percobaan 1

--- jobsheet5/ifCetakKRS08.java
+++ jobsheet5/ifCetakKRS08.java
@@ -7,9 +7,13 @@ public class ifCetakKRS08 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas);
-        System.out.println("Pembayaran  UKT terverifikasi");
-        System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Silakan lunasi UKT terlebih dahulu agar bisa mencetak KRS");
+        }
+        sc.close();
     }
     
 }
