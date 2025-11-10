1a68c520c8d0ad2851db7d63de33eeedecd02066
07-Bagaskara <bagaskarapradana36@gmail.com>
2025-11-11 00:02:05 +0700

    ArrayBilangan08.java

---
+++ jobsheet9/ArrayBilangan08.java
@@ -0,0 +1,11 @@
+package jobsheet9;
+
+public class ArrayBilangan08 {
+    public static void main(String[] args) {
+        int[] bil = {5, 13, -7, 17};
+
+         for (int i = 0; i < bil.length ; i++) {
+            System.out.println(bil[i]);   
+        }
+    }
+}
