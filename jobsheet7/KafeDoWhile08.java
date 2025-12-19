7b2b272ea7e77ddab366cf21c4670d2c130f6c8b
07-Bagaskara <bagaskarapradana36@gmail.com>
2025-12-19 09:26:30 +0700

    KafeDoWhile08.java

---
+++ jobsheet7/KafeDoWhile08.java
@@ -0,0 +1,39 @@
+package jobsheet7;
+
+import java.util.Scanner;
+
+public class KafeDoWhile08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int kopi, teh, roti;
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+        String namaPelanggan;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (atau 'batal'): ");
+            namaPelanggan = sc.nextLine();
+
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+
+            int totalHarga = kopi * hargaKopi + teh * hargaTeh + roti * hargaRoti;
+            System.out.println("Total harga: Rp " + totalHarga);
+
+            sc.nextLine(); 
+
+        } while (true);
+        sc.close();
+    }
+}
