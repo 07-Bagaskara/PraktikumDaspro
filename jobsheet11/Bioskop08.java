1d8789ec8f6ddf63299642d7144d6648a22c4923
07-Bagaskara <bagaskarapradana36@gmail.com>
2025-12-24 08:21:52 +0700

    Bioskop08.java

---
+++ jobsheet11/Bioskop08.java
@@ -0,0 +1,34 @@
+package jobsheet11;
+
+public class Bioskop08 {
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+
+        // Mengisi elemen array
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        // Menampilkan panjang baris (Pertanyaan 5)
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        // Menampilkan penonton setiap baris menggunakan String.join (Pertanyaan 9 & 13)
+        System.out.println("Penonton setiap baris:");
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+        }
+    }
+}
